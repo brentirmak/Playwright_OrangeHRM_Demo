97de56264a10aac137cad404d2bf2db2349d320a
brentirmak <brent.irmak@gmail.com>
2026-05-19 16:06:56 -0700

    Add test_orangehrm.py file

---
+++ tests/test_orangehrm.py
@@ -0,0 +1,45 @@
+import time
+
+from playwright.sync_api import Page, expect
+from pages.orangehrm_performance_page import PerformancePage
+from pages.orangehrm_home_page import HomePage
+#from pages.orangehrm_login_page import LoginPage
+from pages.orangehrm_admin_page import AdminPage
+from pages.orangehrm_leave_page import LeavePage
+from pages.orangehrm_logout_page import LogoutPage
+
+def testHomePage(page: Page) -> None:
+    page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    home_page = HomePage(page)
+
+#def testLoginPage(page: Page) -> None:
+    #login_page = LoginPage(page)
+    #login_page.username.fill("Admin")
+    #login_page.password.fill("admin123")
+    #login_page.click_login()
+    #expect(page.get_by_text("manda user")).to_be_visible()
+
+def testPerformancePage(page: Page) -> None:
+    performance_page = PerformancePage(page)
+    performance_page.click_performance_menu()
+    expect(page.get_by_role("heading", name="Employee Reviews")).to_be_visible()
+
+def testAdminPage(page: Page) -> None:
+    admin_page = AdminPage(page)
+    admin_page.click_admin_menu()
+    expect(page.get_by_role("heading", name="System Users")).to_be_visible()
+
+def testLeavePage(page: Page) -> None:
+    testLeavePage = LeavePage(page)
+    testLeavePage.click_leave_menu()
+    expect(page.get_by_role("heading", name="Leave List")).to_be_visible()
+    expect(page.get_by_role("button", name="Search")).to_be_visible()
+
+def testLogout(page: Page) -> None:
+    logout_page = LogoutPage(page)
+    time.sleep(2)
+    logout_page.click_logout()
+    time.sleep(2)
+    expect(page.get_by_role("button", name="Login")).to_be_visible()
+
+
