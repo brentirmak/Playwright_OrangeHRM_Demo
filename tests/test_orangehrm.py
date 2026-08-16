40f106b4c80648571703357e9ed88e8904b72521
brentirmak <brent.irmak@gmail.com>
2026-08-15 21:45:52 -0700

    Add comments/print statements for easier tracing

--- tests/test_orangehrm.py
+++ tests/test_orangehrm.py
@@ -9,7 +9,9 @@ from pages.orangehrm_leave_page import LeavePage
 from pages.orangehrm_logout_page import LogoutPage
 
 def test_HomePage(page: Page) -> None:
+    print("\nWill go to home page")
     page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    print("Loaded home page")
     home_page = HomePage(page)
 
 #def test_LoginPage(page: Page) -> None:
@@ -21,25 +23,37 @@ def test_HomePage(page: Page) -> None:
 
 def test_PerformancePage(page: Page) -> None:
     performance_page = PerformancePage(page)
+    print("Will click on performance option on the menu")
     performance_page.click_performance_menu()
+    print("Clicked on performance option on the menu")
     expect(page.get_by_role("heading", name="Employee Reviews")).to_be_visible()
+    print("Verified Employee Reviews heading is visible on the performance page")
 
 def test_AdminPage(page: Page) -> None:
     admin_page = AdminPage(page)
+    print("Will click on admin option on the menu")
     admin_page.click_admin_menu()
+    print("Clicked on admin option on the menu")
     expect(page.get_by_role("heading", name="System Users")).to_be_visible()
+    print("Verified System Users heading is visible on the admin page")
 
 def test_LeavePage(page: Page) -> None:
     testLeavePage = LeavePage(page)
+    print("Will click on leave option on the menu")
     testLeavePage.click_leave_menu()
+    print("Clicked on leave option on the menu")
     expect(page.get_by_role("heading", name="Leave List")).to_be_visible()
+    print("Verified Leave List heading is visible on the leave page")
     expect(page.get_by_role("button", name="Search")).to_be_visible()
+    print("Verified Search button is visible on the leave page")
 
 def test_Logout(page: Page) -> None:
     logout_page = LogoutPage(page)
+    print("Will click on logout option on the menu")
     time.sleep(2)
     logout_page.click_logout()
+    print("Clicked on logout option on the menu")
     time.sleep(2)
     expect(page.get_by_role("button", name="Login")).to_be_visible()
-
+    print("Verified Login button is visible on the login page after logout")
 
