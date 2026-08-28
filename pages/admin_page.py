14abe5511ef2bbb098810f46d6ecea30c7ab155a
brentirmak <brent.irmak@gmail.com>
2026-08-27 17:38:36 -0700

    Added add/delete user transactions

--- pages/admin_page.py
+++ pages/admin_page.py
@@ -3,10 +3,14 @@ class AdminPage:
     def __init__(self, page):
         self.page = page
 
-        # Admin section
+        #####################################################################################################################
+        # Admin section identifiers
+        #####################################################################################################################
         self.admin_menu = page.get_by_role("link", name="Admin")
 
-        # User Management section
+        #####################################################################################################################
+        # User Management section identifiers
+        #####################################################################################################################
         self.search_button = page.get_by_role("button", name="Search")
         self.username_field = page.get_by_role("textbox").nth(1)
         self.reset_button = page.get_by_role("button", name="Reset")
@@ -15,8 +19,18 @@ class AdminPage:
         self.employee_name_field = page.get_by_role("textbox", name="Type for hints...")
         self.status_dropdown = page.get_by_text("Status-- Select --")
         self.enabled_option = page.get_by_role("option", name="Enabled")
-        
-        # Job section
+        self.add_button = page.locator("//button[contains(.,'Add')]")
+        self.user_role_dropdown = page.locator("(//div[@class='oxd-select-text-input'][contains(.,'-- Select --')])[1]")
+        self.employee_name_field_suggestion = page.get_by_text("FName Mname LName")
+        self.create_username_field = page.get_by_role("textbox").nth(2)
+        self.create_password_field = page.get_by_role("textbox").nth(3)
+        self.confirm_create_password_field = page.get_by_role("textbox").nth(4)
+        self.save_button = page.get_by_role("button", name="Save")
+        self.delete_button = page.get_by_role("button", name=" Yes, Delete")
+
+        #####################################################################################################################
+        # Job section indentifiers
+        #####################################################################################################################
         self.job_submenu = page.get_by_role("listitem").filter(has_text="Job")
         self.job_titles_submenu = page.get_by_role("menuitem", name="Job Titles")
         self.pay_grades_submenu = page.get_by_role("menuitem", name="Pay Grades")
@@ -24,13 +38,17 @@ class AdminPage:
         self.job_categories_submenu = page.get_by_role("menuitem", name="Job Categories")
         self.work_shifts_submenu = page.get_by_role("menuitem", name="Work Shifts")
 
-        # Organization section
+        #####################################################################################################################
+        # Organization section identifiers
+        #####################################################################################################################
         self.organization_submenu = page.get_by_text("Organization")
         self.general_information_submenu = page.get_by_role("menuitem", name="General Information")
         self.locations_submenu = page.get_by_role("menuitem", name="Locations")
         self.structure_submenu = page.get_by_role("menuitem", name="Structure")
 
-        # Qualifications section
+        #####################################################################################################################
+        # Qualifications section identifiers
+        #####################################################################################################################
         self.qualifications_submenu = page.get_by_text("Qualifications")
         self.skills_submenu = page.get_by_role("menuitem", name="Skills")
         self.education_submenu = page.get_by_role("menuitem", name="Education")
@@ -38,11 +56,15 @@ class AdminPage:
         self.languages_submenu = page.get_by_role("menuitem", name="Languages")
         self.memberships_submenu = page.get_by_role("menuitem", name="Memberships")
 
+        #####################################################################################################################
         # Nationalities/Corporate Branding section
+        #####################################################################################################################
         self.nationalities_submenu = page.get_by_role("link", name="Nationalities")
         self.corporate_branding_submenu = page.get_by_role("link", name="Corporate Branding")
 
+        #####################################################################################################################
         # Configuration section
+        #####################################################################################################################
         self.configuration_submenu = page.get_by_role("listitem").filter(has_text="Configuration")
         self.email_configuration_submenu = page.get_by_role("menuitem", name="Email Configuration")
         self.email_subscription_submenu = page.get_by_role("menuitem", name="Email Subscriptions")
@@ -53,15 +75,17 @@ class AdminPage:
         self.register_oauth_client_submenu = page.get_by_role("menuitem", name="Register OAuth Client")
         self.ldap_configuration_submenu = page.get_by_role("menuitem", name="LDAP Configuration")
 
-    ################################################
+    #####################################################################################################################
     # Admin page/section methods
+    #####################################################################################################################
     def click_admin_menu(self):
         self.admin_menu.click()
         self.page.wait_for_load_state("networkidle")
     ################################################
 
-    ################################################
+    #####################################################################################################################
     # User Management section methods
+    #####################################################################################################################
     def enter_username(self, username):
         self.username_field.fill(username)
         self.page.wait_for_load_state("networkidle")
@@ -107,9 +131,54 @@ class AdminPage:
         self.enabled_option.click()
         self.page.wait_for_load_state("networkidle")
 
-    
-    ################################################
+    def click_add_button(self):
+        self.add_button.click()
+        self.page.wait_for_load_state("networkidle")
+
+    def click_user_role_dropdown(self):
+        self.user_role_dropdown.click()
+        self.page.wait_for_load_state("networkidle")
+
+    def click_create_username_field(self):
+        self.create_username_field.click()
+        self.page.wait_for_load_state("networkidle")
+
+    def enter_create_username(self):
+        self.create_username_field.fill("TestUser123")
+        self.page.wait_for_load_state("networkidle")
+
+    def click_create_password_field(self):
+        self.create_password_field.click()
+        self.page.wait_for_load_state("networkidle")
+
+    def enter_create_password(self):
+        self.create_password_field.fill("TestUser123")
+        self.page.wait_for_load_state("networkidle")
+
+    def click_confirm_create_password_field(self):
+        self.confirm_create_password_field.click()
+        self.page.wait_for_load_state("networkidle")
+
+    def enter_confirm_create_password(self):
+        self.confirm_create_password_field.fill("TestUser123")
+        self.page.wait_for_load_state("networkidle")
+
+    def click_save_button(self):
+        self.save_button.click()
+        self.page.wait_for_load_state("networkidle")
+
+    def click_trashbin_of_tobedeleted_item(self):
+        row = self.page.locator("div.oxd-table-card").filter(has_text="TestUser123")
+        row.get_by_role("button").filter(has=self.page.locator("i.bi-trash")).click()
+        self.page.wait_for_load_state("networkidle")
+
+    def click_delete_button(self):
+        self.delete_button.click()
+        self.page.wait_for_load_state("networkidle")
+
+    #####################################################################################################################
     # Job section methods
+    #####################################################################################################################
     def click_job_submenu(self):
         self.job_submenu.click()
         self.page.wait_for_load_state("networkidle")
@@ -136,8 +205,9 @@ class AdminPage:
     ################################################
 
 
-    ################################################
+    #####################################################################################################################
     # Organization section methods
+    #####################################################################################################################
     def click_organization_submenu(self):
         self.organization_submenu.click()
         self.page.wait_for_load_state("networkidle")
@@ -154,8 +224,9 @@ class AdminPage:
         self.structure_submenu.click()
         self.page.wait_for_load_state("networkidle")
 
-    ################################################
+    #####################################################################################################################
     # Qualifications section methods
+    #####################################################################################################################
     def click_qualifications_submenu(self):
         self.qualifications_submenu.click()
         self.page.wait_for_load_state("networkidle")
@@ -180,8 +251,9 @@ class AdminPage:
         self.memberships_submenu.click()
         self.page.wait_for_load_state("networkidle")
 
-    ################################################
+    #####################################################################################################################
     # Nationalities/Corporate Branding section methods
+    #####################################################################################################################
     def click_nationalities_submenu(self):
         self.nationalities_submenu.click()
         self.page.wait_for_load_state("networkidle")
@@ -190,8 +262,9 @@ class AdminPage:
         self.corporate_branding_submenu.click()
         self.page.wait_for_load_state("networkidle")
 
-    ################################################
+    #####################################################################################################################
     # Configuration section methods
+    #####################################################################################################################
     def click_configuration_submenu(self):
         self.configuration_submenu.click()
         self.page.wait_for_load_state("networkidle")
