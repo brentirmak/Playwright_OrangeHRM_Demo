c4e3921f064320ca4a8df4c2d1d1ad0b5996d479
brentirmak <brent.irmak@gmail.com>
2026-08-29 14:32:27 -0700

    Added a few more transactions for the Time section to cover all of the main pages

--- tests/test_orangehrm_time.py
+++ tests/test_orangehrm_time.py
@@ -51,8 +51,6 @@ def test_AccessTimeSection(shared_page):
     print("Clicked on the My Timesheets submenu")
     expect(shared_page.get_by_role("heading", name="My Timesheet")).to_be_visible()
     print("Verified that the My Timesheet header was displayed")
-    expect(shared_page.get_by_role("button", name="Submit")).to_be_visible()
-    print("Verified that the Submit header was displayed")
     print("\nEnded test_AccessTimeSection transaction")
 
 def test_AccessAttendanceMyRecordsPage(shared_page):
@@ -103,9 +101,67 @@ def test_AccessAttendanceConfigurationPage(shared_page):
     print("Verified that the Attendance Configuration header was displayed")
     print("\nEnded test_AccessAttendanceConfigurationPage transaction")
 
+def test_AccessReportsProjectReportsPage(shared_page):
+    print("\nStarting test_AccessReportsProjectReportsPage transaction")
+    time_page = TimePage(shared_page)
+    print("Will click on the Reports submenu")
+    time_page.click_reports_submenu()
+    print("Clicked on the Reports submenu")
+    time_page.click_project_reports_submenu()
+    print("Clicked on the Project Reports submenu")
+    expect(shared_page.get_by_role("heading", name="Project Report")).to_be_visible()
+    print("Verified that the Project Report header was displayed")
+    print("\nEnded test_AccessReportsProjectReportsPage transaction")
 
+def test_AccessReportsEmployeeReportsPage(shared_page):
+    print("\nStarting test_AccessReportsEmployeeReportsPage transaction")
+    time_page = TimePage(shared_page)
+    print("Will click on the Reports submenu")
+    time_page.click_reports_submenu()
+    print("Clicked on the Reports submenu")
+    time_page.click_employee_reports_submenu()
+    print("Clicked on the Employee Reports submenu")
+    expect(shared_page.get_by_role("heading", name="Employee Report")).to_be_visible()
+    print("Verified that the Employee Report header was displayed")
+    print("\nEnded test_AccessReportsEmployeeReportsPage transaction")
 
+def test_AccessReportsAtendanceSummaryPage(shared_page):
+    print("\nStarting test_AccessReportsAtendanceSummaryPage transaction")
+    time_page = TimePage(shared_page)
+    print("Will click on the Reports submenu")
+    time_page.click_reports_submenu()
+    print("Clicked on the Reports submenu")
+    time_page.click_attendance_summary_submenu()
+    print("Clicked on the Attendance Summary submenu")
+    expect(shared_page.get_by_role("heading", name="Attendance Total Summary")).to_be_visible()
+    print("Verified that the Attendance Total Summary header was displayed")
+    print("\nEnded test_AccessReportsAtendanceSummaryPage transaction")
 
+def test_AccessProjectInfoCustomersPage(shared_page):
+    print("\nStarting test_AccessProjectInfoCustomersPage transaction")
+    time_page = TimePage(shared_page)
+    print("Will click on the Projects Info submenu")
+    time_page.click_project_info_submenu()
+    print("Will click on the Customers submenu")
+    time_page.click_customers_submenu()
+    print("Clicked on the Customers submenu")
+    expect(shared_page.get_by_role("heading", name="Customers")).to_be_visible()
+    print("Verified that the Customers heading was displayed")
+    expect(shared_page.get_by_text("Records Found")).to_be_visible()
+    print("Verified that the Records Found text was displayed")
+    print("\nEnded test_AccessProjectInfoCustomersPage transaction")
+
+def test_AccessProjectInfoProjectsPage(shared_page):
+    print("\nStarting test_AccessProjectInfoProjectsPage transaction")
+    time_page = TimePage(shared_page)
+    print("Will click on the Projects Info submenu")
+    time_page.click_project_info_submenu()
+    print("Will click on the Projects submenu")
+    time_page.click_projects_submenu()
+    print("Clicked on the Projects submenu")
+    expect(shared_page.get_by_role("heading", name="Projects")).to_be_visible()
+    print("Verified that the Projects heading was displayed")
+    print("\nEnded test_AccessProjectInfoProjectsPage transaction")
 
 def test_Logout(shared_page):
     print("\nStarting test_Logout transaction")
