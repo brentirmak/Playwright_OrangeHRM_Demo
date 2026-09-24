2e6b377d179eadc258b2fd0d9e2e19561c0626a2
brentirmak <brent.irmak@gmail.com>
2026-09-24 14:12:21 -0700

    Added timeout values for sync points

--- tests/test_orangehrm_time.py
+++ tests/test_orangehrm_time.py
@@ -38,7 +38,7 @@ def test_AccessTimeSection(shared_page):
     print("Clicked on the timesheets submenu")
     time_page.click_my_timesheets_submenu()
     print("Clicked on the My Timesheets submenu")
-    expect(shared_page.get_by_role("heading", name="My Timesheet")).to_be_visible()
+    expect(shared_page.get_by_role("heading", name="My Timesheet")).to_be_visible(timeout=10000)
     print("Verified that the My Timesheet header was displayed")
     print("\nEnded test_AccessTimeSection transaction")
 
@@ -50,7 +50,7 @@ def test_AccessAttendanceMyRecordsPage(shared_page):
     print("Clicked on the Attendance submenu")
     time_page.click_my_records_submenu()
     print("Clicked on the My Records submenu")
-    expect(shared_page.get_by_role("heading", name="My Attendance Records")).to_be_visible()
+    expect(shared_page.get_by_role("heading", name="My Attendance Records")).to_be_visible(timeout=10000)
     print("Verified that the My Attendance Records header was displayed")
     print("\nEnded test_AccessAttendanceMyRecordsPage transaction")
 
@@ -62,7 +62,7 @@ def test_AccessAttendancePunchInOutPage(shared_page):
     print("Clicked on the Attendance submenu")
     time_page.click_punchin_punchout_submenu()
     print("Clicked on PunchIn/Punchout submenu")
-    expect(shared_page.get_by_role("heading", name="Punch In")).to_be_visible()
+    expect(shared_page.get_by_role("heading", name="Punch In")).to_be_visible(timeout=10000)
     print("Verified that the Punch In header was displayed")
     print("\nEnded test_AccessAttendancePunchInOutPage transaction")
 
@@ -74,7 +74,7 @@ def test_AccessAttendanceEmployeeRecordsPage(shared_page):
     print("Clicked on the Attendance submenu")
     time_page.click_employee_records_submenu()
     print("Clicked on the Employee Records submenu")
-    expect(shared_page.get_by_role("heading", name="Employee Attendance Records")).to_be_visible()
+    expect(shared_page.get_by_role("heading", name="Employee Attendance Records")).to_be_visible(timeout=10000)
     print("Verified that the Employee Attendance Reocrds header was displayed")
     print("\nEnded test_AccessAttendanceEmployeeRecordsPage transaction")
 
@@ -86,7 +86,7 @@ def test_AccessAttendanceConfigurationPage(shared_page):
     print("Clicked on the Attendance submenu")
     time_page.click_configuration_submenu()
     print("Clicked on the Configuration submenu")
-    expect(shared_page.get_by_role("heading", name="Attendance Configuration")).to_be_visible()
+    expect(shared_page.get_by_role("heading", name="Attendance Configuration")).to_be_visible(timeout=10000)
     print("Verified that the Attendance Configuration header was displayed")
     print("\nEnded test_AccessAttendanceConfigurationPage transaction")
 
@@ -98,7 +98,7 @@ def test_AccessReportsProjectReportsPage(shared_page):
     print("Clicked on the Reports submenu")
     time_page.click_project_reports_submenu()
     print("Clicked on the Project Reports submenu")
-    expect(shared_page.get_by_role("heading", name="Project Report")).to_be_visible()
+    expect(shared_page.get_by_role("heading", name="Project Report")).to_be_visible(timeout=10000)
     print("Verified that the Project Report header was displayed")
     print("\nEnded test_AccessReportsProjectReportsPage transaction")
 
@@ -110,7 +110,7 @@ def test_AccessReportsEmployeeReportsPage(shared_page):
     print("Clicked on the Reports submenu")
     time_page.click_employee_reports_submenu()
     print("Clicked on the Employee Reports submenu")
-    expect(shared_page.get_by_role("heading", name="Employee Report")).to_be_visible()
+    expect(shared_page.get_by_role("heading", name="Employee Report")).to_be_visible(timeout=10000)
     print("Verified that the Employee Report header was displayed")
     print("\nEnded test_AccessReportsEmployeeReportsPage transaction")
 
@@ -122,7 +122,7 @@ def test_AccessReportsAtendanceSummaryPage(shared_page):
     print("Clicked on the Reports submenu")
     time_page.click_attendance_summary_submenu()
     print("Clicked on the Attendance Summary submenu")
-    expect(shared_page.get_by_role("heading", name="Attendance Total Summary")).to_be_visible()
+    expect(shared_page.get_by_role("heading", name="Attendance Total Summary")).to_be_visible(timeout=10000)
     print("Verified that the Attendance Total Summary header was displayed")
     print("\nEnded test_AccessReportsAtendanceSummaryPage transaction")
 
@@ -134,9 +134,9 @@ def test_AccessProjectInfoCustomersPage(shared_page):
     print("Will click on the Customers submenu")
     time_page.click_customers_submenu()
     print("Clicked on the Customers submenu")
-    expect(shared_page.get_by_role("heading", name="Customers")).to_be_visible()
+    expect(shared_page.get_by_role("heading", name="Customers")).to_be_visible(timeout=10000)
     print("Verified that the Customers heading was displayed")
-    expect(shared_page.get_by_text("Records Found")).to_be_visible()
+    expect(shared_page.get_by_text("Records Found")).to_be_visible(timeout=10000)
     print("Verified that the Records Found text was displayed")
     print("\nEnded test_AccessProjectInfoCustomersPage transaction")
 
@@ -148,7 +148,7 @@ def test_AccessProjectInfoProjectsPage(shared_page):
     print("Will click on the Projects submenu")
     time_page.click_projects_submenu()
     print("Clicked on the Projects submenu")
-    expect(shared_page.get_by_role("heading", name="Projects")).to_be_visible()
+    expect(shared_page.get_by_role("heading", name="Projects")).to_be_visible(timeout=10000)
     print("Verified that the Projects heading was displayed")
     print("\nEnded test_AccessProjectInfoProjectsPage transaction")
 
@@ -158,6 +158,6 @@ def test_Logout(shared_page):
     print("Will click on logout option on the menu")
     logout_page.click_logout()
     print("Clicked on logout option on the menu")
-    expect(shared_page.get_by_role("button", name="Login")).to_be_visible()
+    expect(shared_page.get_by_role("button", name="Login")).to_be_visible(timeout=10000)
     print("Verified Login button is visible on the login page after logout")
     print("Ended test_Logout transaction")
